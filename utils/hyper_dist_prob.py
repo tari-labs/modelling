2dc0f0ca38aacd3baf7b43715ed35e1792042ccb
Hansie Odendaal <pluto@tari.com>
2019-03-29 14:53:42 +0200

    Windows compatibility for type checking

--- utils/hyper_dist_prob.py
+++ utils/hyper_dist_prob.py
@@ -12,6 +12,7 @@ get_ipython().magic('clear')
 
 import numpy as np
 import math
+import operator
 from decimal import Decimal
 
 def nCk(n,k): 
@@ -20,40 +21,44 @@ def nCk(n,k):
     return np.float64((Decimal(math.factorial(n)) / Decimal(math.factorial(n - k))) / Decimal(math.factorial(k)))
 
 def probability(type_threshold, no_of_type_in_set, sample_size, set_size) -> np.float64:
-    #Type checking - Python does not have built-in type checking 
-    
+    #Type checking - Python does not have built-in type checking   
+    type_error = False
     ##type_threshold
-    if (type(type_threshold) == 'np.int64' or type(type_threshold) == 'int'):
+    try:
+        operator.index(type_threshold)
         T = np.int_(type_threshold)
-    else:
-        print('Error: \'type_threshold\' - expected int/np.int64, received %s' % (type(type_threshold)))
-        return 0
+    except TypeError:
+        print('Error: \'type_threshold\' - expected int (or int variant), received %s' % (type(type_threshold)))
+        type_error = True
     
     ##no_of_type_in_set
-    if (type(no_of_type_in_set) == 'np.int64' or type(no_of_type_in_set) == 'int'):
-        T = np.int_(no_of_type_in_set)
-    else:
-        print('Error: \'no_of_type_in_set\' - expected int/np.int64, received %s' % (type(no_of_type_in_set)))
-        return 0
+    try:
+        operator.index(no_of_type_in_set)
+        m = np.int_(no_of_type_in_set)
+    except TypeError:
+        print('Error: \'no_of_type_in_set\' - expected int (or int variant), received %s' % (type(no_of_type_in_set)))
+        type_error = True
     
     ##sample_size
-    if (type(sample_size) == 'np.int64' or type(sample_size) == 'int'):
-        T = np.int_(sample_size)
-    else:
-        print('Error: \'sample_size\' - expected int/np.int64, received %s' % (type(sample_size)))
-        return 0
+    try:
+        operator.index(sample_size)
+        n = np.int_(sample_size)
+    except TypeError:
+        print('Error: \'sample_size\' - expected int (or int variant), received %s' % (type(sample_size)))
+        type_error = True
     
     ##set_size
-    if (type(set_size) == 'np.int64' or type(set_size) == 'int'):
-        T = np.int_(set_size)
-    else:
-        print('Error: \'set_size\' - expected int/np.int64, received %s' % (type(set_size)))
+    try:
+        operator.index(set_size)
+        N = np.int_(set_size)
+    except TypeError:
+        print('Error: \'set_size\' - expected int (or int variant), received %s' % (type(set_size)))
+        type_error = True
+    
+    if type_error:
         return 0
     
-    
-    m = no_of_type_in_set
-    N = set_size
-    n = sample_size
+    #Calculation
     P_tot = 0
     #Calculate probability
     for i in range(T, n + 1):
