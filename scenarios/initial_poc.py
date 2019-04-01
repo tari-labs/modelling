3b023ec4bd27a3ec12a690eb62839bc8527a6a4f
Kevoulee <39254222+Kevoulee@users.noreply.github.com>
2019-04-01 08:34:37 +0200

    error fixes and graph modification (#2)
    * error fixes and graph modification
    
    * Update scenarios/initial_poc.py
    
    Co-Authored-By: Kevoulee <39254222+Kevoulee@users.noreply.github.com>
    
    * Update scenarios/initial_poc.py
    
    Co-Authored-By: Kevoulee <39254222+Kevoulee@users.noreply.github.com>

--- scenarios/initial_poc.py
+++ scenarios/initial_poc.py
@@ -21,28 +21,20 @@ import hyper_dist_prob as hdp
 import numpy as np
 from IPython import get_ipython
 get_ipython().magic('clear')
-#get_ipython().magic('reset -sf')
 
-import matplotlib.pyplot as plt
 
-# Declare and create an array with the total nodes (N) 
+# Declare and initialize variables 
 
 no_of_nodes = []
 
-## Initialize first array value 
-
-
-## Initialize the array size 
-
 no_of_nodes_start = 1  
-no_of_nodes_max = 100
-array_size = 5  
+no_of_nodes_max = 100 
 
-## Initialize increment 
+## Node increment 
 
-incr = 1 
+incr = 1                 
 
-## Create the array 
+## Create nodes array 
 
 no_of_nodes = np.arange(no_of_nodes_start, no_of_nodes_max + 1, incr)
 
@@ -65,17 +57,15 @@ bft_threshold_percentage = 0.5
 
 # Populate input arrays 
 
-## For each index in the array:
-
-### Calculate the number of Bad actors (product of total nodes (N) and Bad actors percentage, round down)
+## Calculate the number of Bad actors (product of total nodes (N) and Bad actors percentage, round down)
 
 no_of_bad_actors = np.int_(np.floor(no_of_nodes * bad_actors_percentage))
 
-### Calculate the committee size (product of Total nodes (N) and Committee size percentage, round up)
+## Calculate the committee size (product of Total nodes (N) and Committee size percentage, round up)
 
 committee_size = np.int_(np.ceil(no_of_nodes * committee_size_percentage))
 
-### Calculate the BFT threshold (product of committee size (n) and BFT threshold percentage, round up)
+## Calculate the BFT threshold (product of committee size (n) and BFT threshold percentage, round up)
 
 bft_threshold = np.int_(np.ceil(committee_size * bft_threshold_percentage))
 
@@ -83,19 +73,24 @@ bft_threshold = np.int_(np.ceil(committee_size * bft_threshold_percentage))
 # up to the committee size 
 
 P_tot = []
+print('Tot_nodes Bad_nodes Committee Threshold  index   Probability to control')
 for j in range(0, len(no_of_nodes)):
-    P_tot.append(hdp.probability(bft_threshold, no_of_bad_actors, committee_size, no_of_nodes))
-    print(' %3s  %3s  %3s  %3s    j=%3s     P_tot= %-20s' % \
+    P_tot.append(hdp.probability(bft_threshold[j], no_of_bad_actors[j], committee_size[j], no_of_nodes[j]))
+    print('    %3s      %3s        %3s     %3s      j=%3s   P_tot= %-20s' % \
         (no_of_nodes[j], no_of_bad_actors[j], committee_size[j], bft_threshold[j], j, P_tot[j]))
 
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
 
-fig, ax1 = plt.subplots(figsize=(12,12))   
+#Plots
+
+fig, ax1 = plt.subplots(figsize=(12,9))   
+ax2 = ax1.twinx()
 
 ax1.set_xlabel('Total of Network Nodes', fontsize='18')
 ax1.set_ylabel('Probability of bad actors controlling the network', fontsize='18')
+ax2.set_ylabel('Number of nodes', fontsize='18')
 ax1.tick_params(axis='both', labelsize='14')
+ax2.tick_params(axis='both', labelsize='14')
  
 ax1.grid(True, linestyle='-.')
 ax1.xaxis.grid(True, which='minor', linestyle='-.')
@@ -106,9 +101,9 @@ y2 = no_of_bad_actors
 y3 = committee_size
 y4 = bft_threshold
 
-ax2 = ax1.twinx()
 ax1.plot(x, y1, 'g-', label='Probability')
-ax1.plot(x, y2, 'b-', label='No of bad actors')
-ax1.plot(x, y3, 'r-', label='Committee size')
-ax1.plot(x, y4, 'y-', label='BFT threshold')
-ax1.legend(fontsize='16')
+ax2.plot(x, y2, 'b-', label='No of bad actors')
+ax2.plot(x, y3, 'r-', label='Committee size')
+ax2.plot(x, y4, 'y-', label='BFT threshold')
+ax1.legend(fontsize='16', loc=3)
+ax2.legend(fontsize='16', loc=4)
