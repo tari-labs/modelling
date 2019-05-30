10cf52bfca9291419af0784226b8ab1239c24247
Kevoulee <kev@tari.com>
2019-05-30 13:06:38 +0200

    addition of histogram

--- scenarios/new-monte-carlo-test.py
+++ scenarios/new-monte-carlo-test.py
@@ -4,34 +4,72 @@
 Created on Tue May 28 11:37:11 2019
 
 What is the probability of selecting a majority of bad nodes from a total of 
-5 nodes if the committee size is 3. There are 3 bad nodes and 2 good nodes 
+8 nodes if the committee size is 3. There are 4 bad nodes and 4 good nodes. 
+
+We perform N experiments, count how many times we get two or more bad nodes 
+(M) and estimate the probability M/N.
+
+Eash experment consists of making the total_nodes list, drawing a number of 
+nodes and counting how many bad nodes were drawn.
 
 @author: kevoulee
 """
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-def total_nodes(): #make total_nodes with 5 nodes 
-    total_nodes = []
-    for node_type in 'bad' 'good':
- #       for i in the range(total_nodes):
-            total.append(node_type)
-    return total 
+#node_type classifies a node as being good or bad
 
-def committee_size(total):
-    index = random.randint(0, len(total_nodes))
-    node_type = total_nodes[index];  del total_nodes[index]
-    return node_type
+def draw_node(total_nodes):
+#Draw a node using list index
+    index = random.randint(0, len(total_nodes)-1)
+    node_type = total_nodes.pop(index)
+    return node_type, total_nodes
 
-# run experiments:
-n = input('How many nodes in the committee_size? ')
-N = input('How many experiments? ')
+def new_nodes(total_nodes, bad_nodes, node_types):
+    total_nodes_vec = []
+    for i in range(total_nodes):
+        total_nodes_vec.append(node_types[0])
+    if bad_nodes > total_nodes: 
+        return total_nodes_vec
+    i = 0
+    while i < bad_nodes:
+        index = random.randint(0, total_nodes-1)
+        if total_nodes_vec[index] != node_types[1]:
+            total_nodes_vec[index] = node_types[1] 
+            i += 1
+    return total_nodes_vec
+
+
+total_nodes = int(input('What is the total amount of nodes?'))
+bad_nodes = int(input('What is the amount of bad nodes?'))
+bft_threshold = int(input('What is the BFT threshold?'))
+n = int(input('How many nodes are to be drawn? '))
+N = int(input('How many experiments? '))
+
+
+# Run experiments
+node_types = 'good', 'bad'
 M = 0  # no of successes
-
+random_test = np.int64(np.zeros((2, total_nodes)))
 for e in range(N):
-    nodes = []         # the n nodes we draw
+    nodes = new_nodes(total_nodes, bad_nodes, node_types)
+    for i in range(total_nodes):
+        if nodes[i] == node_types[0]:
+           random_test[0][i] += 1
+        else:
+            random_test[1][i] += 1
+    print (nodes)
     for i in range(n):
-        node_type, total_nodes = commitee_size(total_nodes)
-        nodes.append(node_type)
-    if nodes.count('bad') >= 2:  # two bad nodes or more
+        node_type, nodes = draw_node(nodes)
+        print (nodes)
+    if nodes.count('bad') >= bft_threshold: 
         M += 1
-print 'Probability:', float(M)/N
+print ('Probability', float(M)/N)
+
+
+print (random_test)
+
+plt.hist(random_test, bins='auto')
+plt.title("histogram with 'auto'bins")
+plt.show()
