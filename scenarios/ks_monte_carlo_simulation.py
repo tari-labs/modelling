b1d142b729bea6bd5ca7085417f07896ec63f41c
Kevoulee <kev@tari.com>
2019-07-11 12:18:38 +0200

    Refactoring and graph presentation

--- scenarios/ks_monte_carlo_simulation.py
+++ scenarios/ks_monte_carlo_simulation.py
@@ -1,160 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 31 13:58:06 2019
+import sys, os
 
-@author: kevoulee
-"""
+#Add ../utils to the Python system path
+try:
+    sys.path.index(os.getcwd().split(os.getcwd().split(os.sep)[-1])[0] + 'utils');
+except ValueError:
+    sys.path.append(os.getcwd().split(os.getcwd().split(os.sep)[-1])[0] + 'utils');
 
-import random
+import network_setup as n_s
+
 import numpy as np
 import matplotlib.pyplot as plt
 from statistics import mean, median, mode, stdev, StatisticsError
+from scipy.stats import linregress
 
-#-----------------------------------------------------------------------------
-#Class definitions
-#-----------------------------------------------------------------------------
-
-# ???
-class NetworkPosition:
-	def __init__(self, radius = 1, maxRangeX = 2500, maxRangeY = 2500, qty = 100):
-		self.radius = radius 
-		self.rangeX = (0, maxRangeX) 
-		self.rangeY = (0, maxRangeY) 
-		self.qty = qty  
-        
-	#Generate a set of all points within `radius` of the origin
-	def generate_coordinates(self):
-		deltas = set()
-		for x in range(-self.radius, self.radius+1): 
-			for y in range(-self.radius, self.radius+1): 
-				if x*x + y*y <= self.radius*self.radius: 
-					deltas.add((x,y))
-		randPoints = []
-		excluded = set()
-		i = 0
-		while i < self.qty:
-			x = random.randrange(*self.rangeX)  
-			y = random.randrange(*self.rangeY) 
-			if (x,y) in excluded: continue 
-			randPoints.append((x,y))
-			i += 1
-			excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
-		return randPoints
-    
-# ???
-class node:
-    malicious = False
-    node_id = -1
-    index = -1
-    def __init__(self, networkPosition = (0, 0)):
-        self.NetworkPosition = networkPosition
-        
 #-----------------------------------------------------------------------------
 #Function definitions
 #-----------------------------------------------------------------------------
 
-# ???
-def create_network(total_nodes, distribution_type):
-    #Create basic nodes in the network
-    _np = NetworkPosition(qty = total_nodes)
-    randPoints = _np.generate_coordinates()
-    network_nodes = [node(networkPosition = randPoints[i]) for i in range(total_nodes)]
-    vec = [-1 for i in range(total_nodes)] #create an empty vector
-    exit_condition = np.int64(0)
-    #Assign node_id to the node
-    vec = [-1 for i in range(total_nodes)] #create an empty vector
-    for i in range(len(network_nodes)): 
-        while vec[i] < 0 and exit_condition < len(network_nodes) * 1000:
-            exit_condition += 1      
-            node_id = get_random_index(distribution_type, 1e6, 1e7)
-            if vec.count(node_id) == 0: #If the number has not been repeated 
-                network_nodes[i].node_id = node_id
-                vec[i] = node_id
-    #Assign index to the node
-    for i in range(len(network_nodes)): #iterate to the range of the network nodes length 
-        network_nodes[i].index = i
-    #return assigned vector of nodes
-    return network_nodes
-
-# ???
-def get_random_index(distribution_type, lower_bound, upper_bound):
-    index = np.int64(0)
-    if distribution_type == "uniform_ditribution":  #use global constant/enum for this?
-        index = random.randint(lower_bound, upper_bound)
-    elif distribution_type == "normal":
-        index = random.randint(lower_bound, upper_bound)  # ToDo: change to normal
-    else:
-        index = random.randint(lower_bound, upper_bound)  # ToDo: Add more options here
-    return index
-
-# ???
-def assign_bad_nodes(network_nodes, bad_nodes, distribution_type):
-    # If all nodes are bad
-    if bad_nodes >= len(network_nodes) :
-        for i in range(len(network_nodes)):
-            network_nodes[i].malicious = True
-        return network_nodes
-    #If some nodes are bad
-    exit_condition = np.int64(0)
-    count = 0
-    while count < bad_nodes and exit_condition < len(network_nodes) * 1000:
-        exit_condition += 1
-        index = get_random_index(distribution_type, 0, len(network_nodes)-1)
-        if network_nodes[index].malicious != True:
-            network_nodes[index].malicious = True
-            count += 1
-    #print ('\nrepeat_factor: ', (exit_condition/len(network_nodes)))
-    return network_nodes
-
-# ???
-def assign_committee(network_nodes, committee_size, distribution_type):
-    # If committee size = total nodes in network
-    if committee_size >= len(network_nodes) :
-        return network_nodes
-    #If committee size < total nodes in network
-    exit_condition = np.int64(0)
-    count = 0
-    committee = []
-    while count < committee_size-1 and exit_condition < len(network_nodes) * 1000:
-        exit_condition += 1
-        #Draw node at random
-        node_index = get_random_index(distribution_type, 0, len(network_nodes)-1)
-        #Assign node if committee size = 0
-        if len(committee) < 1:
-            committee.append(network_nodes[node_index])
-        #Assign node if not already assigned
-        else:
-            found = False
-            #Test if node already assigned
-            for i in range(len(committee)):
-                if committee[i].node_id == network_nodes[node_index].node_id:
-                    found = True
-                    break
-            #Assign node to committee if not assigned
-            if found == False:
-                committee.append(network_nodes[node_index])
-                count += 1
-    #Test if duplicates exist
-    if len(committee) != len(set(committee)):
-        committee = []
-        print('Error! Duplicates nodes found in the proposed committee, committee not assigned.')
-    return committee
-
-#???
-def run_experiment(nodes, committe_size, bft_threshold, no_of_draws) -> np.float64:
+# Run Experiment populating histogram of randomness 
+def run_experiment(nodes, histogram, committe_size, bft_threshold, no_of_draws) -> np.float64:
     M = np.float64(0)
     for m in range(no_of_draws):
         # Create a new committee
-        committee = assign_committee(nodes, committe_size, "uniform_ditribution")
+        committee = n_s.assign_committee(nodes, committe_size, "uniform_ditribution")
         # Count number of times each node has been assigned to a committee
         for i in range(len(committee)):  
             j = committee[i].index
             if committee[i].malicious == True:  
-                histogram_of_randomness[0][j] += 1
+                histogram[0][j] += 1
             else:
-                histogram_of_randomness[1][j] += 1
-            histogram_of_randomness[2][j] += 1
+                histogram[1][j] += 1
+            histogram[2][j] += 1
         # Test of committee has more malicious nodes than BFT threshold
         count = 0
         for i in range(len(committee)):
@@ -162,103 +38,94 @@ def run_experiment(nodes, committe_size, bft_threshold, no_of_draws) -> np.float
                 count += 1
         if count >= bft_threshold: 
             M += 1
-    return M/no_of_draws #calculated probability
+        probability = M/no_of_draws
+    return probability, histogram
 
+
+def plotSettings(figure_size = (12,9), line_style_grid = '-.', \
+                 line_style_axis = '-.', title = " ", x_label = '', y_label = '',title_font_size = '20',font_size = '18', fontweight="bold"):
+    ## Standard graph settings 
+    fig, ax1 = plt.subplots(figsize=figure_size)   
+    ax1.grid(True, linestyle=line_style_grid)
+    ax1.xaxis.grid(True, which='minor', linestyle=line_style_axis)
+    ax1.set_title(title, fontsize=title_font_size, fontweight=fontweight)
+    ax1.set_xlabel(x_label, fontsize=font_size)
+    ax1.set_ylabel(y_label, fontsize=font_size)
+    
 #-----------------------------------------------------------------------------
 #Main program
 #-----------------------------------------------------------------------------
+debug = False
 
 total_nodes = int(input('What is the total amount of nodes? '))
 no_of_bad_nodes = int(input('What is the amount of bad nodes? '))
 committe_size = int(input('How many nodes are to be drawn? '))
 bft_threshold = int(input('What is the BFT threshold within the committee? '))
 no_of_draws = int(input('What is the no of draws within an experiment? '))
-no_of_experiments = int(input('How many experiments?')) 
+no_of_experiments = int(input('How many experiments? ')) 
 print("\n\n")
 
 histogram_of_randomness = np.int64(np.zeros((3, total_nodes)))
-nodes = create_network(total_nodes, "uniform_ditribution")
-nodes = assign_bad_nodes(nodes, no_of_bad_nodes, "uniform_ditribution")
+nodes = n_s.create_network(total_nodes, "uniform_ditribution")
+nodes = n_s.assign_bad_nodes(nodes, no_of_bad_nodes, "uniform_ditribution")
 
 #Run the experiments
-x = list(range(no_of_experiments))
 probabilities = []
 average_prob = 0;
 convergence = []
 for i in range(no_of_experiments):
-    probability = run_experiment(nodes, committe_size, bft_threshold, no_of_draws)
+    probability, histogram_of_randomness = run_experiment(nodes, histogram_of_randomness, \
+                                                          committe_size, bft_threshold, no_of_draws)
     average_prob=((average_prob*i)+probability)/(i+1)
     probabilities.append(probability)
     convergence.append(average_prob)
-
-#Graph of no of experiments vs probability to see convergence (law of large numbers)
-
-## Standard graph settings 
-fig, ax1 = plt.subplots(figsize=(12,9))   
-ax1.grid(True, linestyle='-.')
-ax1.xaxis.grid(True, which='minor', linestyle='-.')
-
-ax1.set_xlabel('No. of Experiments', fontsize='18')
-ax1.set_ylabel('Probabilities', fontsize='18')
-plt.plot(x,probabilities)
+    
+# Plot individual probabilities  
+plotSettings(title= "Each experiment's individual probability" ,x_label= 'Experiment No.', y_label= 'Probability')
+x = list(range(no_of_experiments))
+plt.plot(x, probabilities, marker=".")
 
 # Fit line of best fit for all data
-plt.plot(np.unique(x), np.poly1d(np.polyfit(x, probabilities, 1))(np.unique(x)))
+if len(x) > 1:
+    plt.plot(np.unique(x), np.poly1d(np.polyfit(x, probabilities, 1))(np.unique(x)))
 
-# Analysis of best fit line
-#print('intercept: ', )
-# Histogram of randomness  
+# Analysis of line of best fit 
+    slope, intercept, r_value, p_value, std_err = linregress([x], probabilities)
+    print("\n\n")
+    print('Slope:', slope)
+    print('Intercept:', intercept)
+    print('Regression Analysis:', r_value)
 
-print (histogram_of_randomness)
-
-## Standard graph settings 
-fig, ax1 = plt.subplots(figsize=(12,9))   
-ax1.grid(True, linestyle='-.')
-ax1.xaxis.grid(True, which='minor', linestyle='-.')
-
-ax1.set_xlabel('Index', fontsize='18')
-ax1.set_ylabel('histogram_of_randomness', fontsize='18')
-
-x = np.arange(0,len(histogram_of_randomness[2]))
-y = histogram_of_randomness[2]
-
-plt.plot(x, y)
+#Debug info
+if debug == True:
+    print(histogram_of_randomness)
+    
+# Plot histogram of randomness  
+plotSettings(title= "Histogram of Randomness", x_label= 'Node Index', y_label= 'Frequency of Node being Drawn')
+plt.plot(np.arange(0,len(histogram_of_randomness[2])), histogram_of_randomness[2])
 plt.ylim((-1, max(histogram_of_randomness[2])))
 plt.legend(loc='best')
 plt.show()
 
-## Standard graph settings 
-fig, ax1 = plt.subplots(figsize=(12,9))   
-ax1.grid(True, linestyle='-.')
-ax1.xaxis.grid(True, which='minor', linestyle='-.')
-
-ax1.set_xlabel('Index', fontsize='18')
-ax1.set_ylabel('Probabilities', fontsize='18')
-
-x = np.arange(0,len(convergence))
-y = convergence
-
-plt.plot(x, y)
+# Plot convergence  
+plotSettings(title= "Convergence proving LLN ", x_label= 'Experiment No.', y_label= 'Average Probability')
+plt.plot(np.arange(0,len(convergence)), convergence, marker=".")
 plt.legend(loc='best')
 plt.show()
-
 
 # Statistics for histogram of randomness 
 ## Mean 
 stat_mean = mean(np.float64(histogram_of_randomness[2]))
 print('Mean:', stat_mean)
-
 ## Median 
 stat_median = median(np.float64(histogram_of_randomness[2]))
 print('Median:', stat_median)
-
 ## Mode 
 try:
     stat_mode = mode(np.float64(histogram_of_randomness[2]))
     print('Mode', stat_mode)
 except StatisticsError:
-    print("There is no mode!", stat_mode)
-
+    print("There is no mode!")
 ## Standard_deviation
 stat_stdev = stdev(np.float64(histogram_of_randomness[2]))
 print('Standard deviation', stat_stdev)
@@ -272,19 +139,14 @@ for i in range(len(nodes)):
 	    distribution_of_bad_nodes.append(nodes[i].NetworkPosition)
     else:
         distribution_of_good_nodes.append(nodes[i].NetworkPosition)
-print (distribution_of_bad_nodes)
-print("\n\n")
 
-# Distribution of Bad Nodes 
-## Standard graph settings 
-fig, ax1 = plt.subplots(figsize=(12,9))   
-ax1.grid(True, linestyle='-.')
-ax1.xaxis.grid(True, which='minor', linestyle='-.')
+#Debug info
+if debug == True:
+    print (distribution_of_bad_nodes)
+    print("\n\n")
 
-ax1.set_xlabel('X coordinate', fontsize='18')
-ax1.set_ylabel('Y coordinate', fontsize='18')
-
-# Scatter Plot 
+# Plot position of nodes within network
+plotSettings(title= "Position of nodes within Network", x_label= 'X coordinate', y_label= 'Y coordinate')
 plt.scatter(*zip(*distribution_of_good_nodes), color = 'blue', label = 'good')
 plt.scatter(*zip(*distribution_of_bad_nodes), color = 'red', label = 'bad')
 plt.legend(loc='best')
